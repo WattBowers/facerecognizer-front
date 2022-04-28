6b3370be3f61911304e5fc314e5576bd42eb6ea8
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-04-28 18:36:37 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import bcrypt from 'bcrypt-nodejs'
+import cors from 'cors';
+import knex from 'knex';
+import signin from './controllers/signin.js';
+import register from './controllers/register.js';
+import image from './controllers/image.js';
+import profile from './controllers/profile.js'
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'Miniman009',
+      database : 'smart-brain'
+    }
+  });
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res)=> {
+    res.send('success');
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id' , (req, res) => { profile.handleProfile(req, res)})
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`app is running on port ${process.env.PORT}}`)
+});
