e662ae3a4e5ca88fbbd81800601fc615ff519a9e
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-05-03 09:55:45 -0400

    debugging

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import profile from './controllers/profile.js'
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
     }
   });
