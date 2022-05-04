6e8092415239a0b4b0c754467ce05f8d88b9ee90
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-05-04 10:53:26 -0400

    commiting

--- controllers/register.js
+++ controllers/register.js
@@ -29,6 +29,7 @@ const handleRegister = (req, res, db, bcrypt) => {
     })
     .catch(err => res.status(400).json('unable to join'))
 }
-module.exports = {
-    handleRegister: handleRegister
-  };
+
+export default {
+    handleRegister
+};
