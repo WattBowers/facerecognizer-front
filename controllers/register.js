af478fc59930677f18a008eed3da87686ef8cb85
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-05-04 21:44:48 -0400

    different test

--- controllers/register.js
+++ controllers/register.js
@@ -19,12 +19,15 @@ const handleRegister = (req, res, db, bcrypt) => {
             name: name,
             joined: new Date()
         })
+        .then(user => {
+            res.json(user[0])
+        })
     .then(trx.commit)
     .catch(trx.rollback)
     })    
         
     })
-    .catch(err => res.status(400).json('unable to join'))
+    
 }
 
 export default {
