fd6f582b336557b3d6278afe8dbdb9e2d467d5fe
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-05-07 23:11:02 -0400

    multy box fix

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends React.Component {
   
 
   displayFaceBox = (box) => {
-    this.setState({box: faceList});
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -109,11 +109,8 @@ class App extends React.Component {
             
           })
       }
-      response.outputs[0].data.regions.forEach(element => {
-        faceList.push(this.calculateFaceLocation(element));
-        console.log(faceList);
-      })
-      this.displayFaceBox(faceList);
+      this.displayFaceBox(response.outputs[0].data.regions); 
+      
     })
     .catch(err => console.log(err));
   }
